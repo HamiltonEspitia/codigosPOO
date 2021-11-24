4925af0c35a0169ac35719ef6869790fa113533e
HamiltonE <>
2021-11-24 15:25:55 +0000

    drawString

--- Guia3-2/PrimerasFiguras.java
+++ Guia3-2/PrimerasFiguras.java
@@ -2,21 +2,26 @@ import java.awt.*;
 import javax.swing.JApplet;
 import javax.swing.JFrame;
 
-public class PrimerasFiguras extends JApplet {
 
-  public void paint (Graphics g) {
-    g.drawRect(30,30,80,40);
+
+public class PrimerasFiguras extends JApplet {
+  
+  @Override
+  public void paint(Graphics g) {
+    /*g.drawRect(30,30,80,40);
     g.drawOval(120,30,50,50);
-    g.setColor(Color.black);
+    g.setColor(Color.blue);
     g.fillRect(30,100,80,40);
     g.fillOval(120,100,50,50);
     g.drawLine(30,160,130,170);
     g.drawArc(30,180,50,50,60,40);
-    g.fillArc(120,180,50,50,60,40);
+    g.fillArc(120,180,50,50,0,180);*/
+    g.drawRect(30,30,80,40);
+    g.drawString("hola", 80, 40);
     }
 
   public static void main (String[] args){
-    JFrame frame = new JFrame ("Cargando JApplet");
+    JFrame frame = new JFrame ("H JApplet");
     PrimerasFiguras FigurasApplet = new PrimerasFiguras();
     FigurasApplet.init();
     frame.getContentPane().add(FigurasApplet);
